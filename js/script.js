e31f17e71b8fe354f979af43f288cc1243b3b639
LeeVaughn <dlvprogramming@gmail.com>
2021-08-22 08:26:56 -0700

    Add quotes array

--- js/script.js
+++ js/script.js
@@ -1,15 +1,65 @@
-/************************************************
-Treehouse Techdegree:
-Project 4 - Random Quote Generator
-*************************************************/
-
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community
-
 /*** 
  * `quotes` array 
 ***/
+const quotes = [
+  {
+    quote: "Every great developer you know got there by solving problems they were unqualified to solve until they actually did it.",
+    source: "Patrick McKenzie",
+    category: "#programming"
+},
+{
+  quote: "No one in the brief history of computing has ever written a piece of perfect software. It's unlikely that you'll be the first.",
+  source: "Andy Hunt",
+  category: "#programming"
+},
+{
+  quote: "One of the best programming skills you can have is knowing when to walk away for awhile.",
+  source: "Oscar Godson",
+  category: "#programming"
+},
+{
+  quote: "A good programmer is someone who always looks both ways before crossing a one-way street.",
+  source: "Doug Linder",
+  category: "#programming"
+},
+{
+  quote: "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live.",
+  source: "John Woods",
+  category: "#programming"
+},
+{
+  quote: "Anything worth doing well is worth doing poorly at first.",
+  source: "Ray Congdon",
+  category: "#learning"
+},
+{
+  quote: "First, solve the problem. Then, write the code.",
+  source: "Jack Johnson",
+  category: "#programming"
+},
+{
+  quote: "Any code of your own that you haven’t looked at for six or more months might as well have been written by someone else.",
+  source: "Eagleson's Law",
+  category: "#programming"
+},
+{
+  quote: "Programming isn’t about what you know. It’s about what you can figure out.",
+  source: "Chris Pine",
+  category: "#programming"
+},
+{
+  quote: "First do it, then do it right, then do it better.",
+  source: "Addy Osmani",
+  category: "#success"
+},
+{
+  quote: "Don't use a comment when you can use a function or a variable.",
+  source: "Martin Fowler",
+        category: "#programming",
+        year: 2008,
+        citation: "Clean Code: A Handbook of Agile Software Craftsmanship"
+},
+]
 
 
 
